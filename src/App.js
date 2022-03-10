1a14f3c67f532a3c76aee85e9b95bb9551499c00
anshu <lcb2021043@iiitl.ac.in>
2022-03-10 17:10:26 +0530

    added haeder and styling and contact info

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
-import { Route } from 'react-router-dom';
+import { Switch , Route } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
 
 
 function App() {
   return (
     <div>
-   
+    <Header /> 
+    <Switch>
+
      <Route exact path="/" component= {HomePage} />
      <Route path="/shop" component= {ShopPage} />
+    </Switch>
 
     </div>
   );
